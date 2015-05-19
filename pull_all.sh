8f1fe2bc663fec8dd3fadb60c6aaff6868a5c0b1
Kimbsy <dave.kimber@simitive.com>
2015-05-19 11:58:15 +0100

    scripts adjusted

--- pull_all.sh
+++ pull_all.sh
@@ -1,4 +1,8 @@
 # Copy files from repo onto local machine.
 
+# do a fresh pull on the repo
+cd ~/Tools/save-files
+git pull
+
 # update knights of pen and paper
 cp knights_of_pen_and_paper/data.kopp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
--- push_all.sh
+++ push_all.sh
@@ -2,3 +2,7 @@
 
 # update knights of pen and paper
 cp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp knights_of_pen_and_paper/data.kopp
+
+# push up to remote
+cd ~/Tools/save-files
+git commit -am "save files commited $(date +%s)"
