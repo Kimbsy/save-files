e4fae8bdec2feafe9e9553e8ad22676c1fb8f965
Kimbsy <lordkimber@gmail.com>
2015-05-19 18:50:47 +0100

    save files commited: 1432057847

--- pull_all.sh
+++ pull_all.sh
@@ -5,7 +5,9 @@ cd ~/Tools/save-files
 git pull
 
 # update knights of pen and paper
-cp knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
+# sometimes steamapps is SteamApps
+test -r ~/.steam/steam/SteamApps && cp knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/SteamApps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
+test -r ~/.steam/steam/steamapps && cp knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
 
 # update FTL
 cp faster_than_light/files/* ~/.local/share/FasterThanLight/
--- push_all.sh
+++ push_all.sh
@@ -1,7 +1,9 @@
 # Copy files from local machine into repo.
 
 # update knights of pen and paper
-cp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp knights_of_pen_and_paper/files/data.kopp
+# for some reason steamapps is sometimes SteamApps
+test -r ~/.steam/steam/SteamApps && cp ~/.steam/steam/SteamApps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp
+test -r ~/.steam/steam/steamapps && cp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp
 
 # update FTL
 cp ~/.local/share/FasterThanLight/* faster_than_light/files/
