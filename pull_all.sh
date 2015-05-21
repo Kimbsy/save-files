a33df74b765f3563d6afab74e1ba239578734ab3
Kimbsy <dave.kimber@simitive.com>
2015-05-21 15:31:39 +0100

    save files commited: 1432218699

--- pull_all.sh
+++ pull_all.sh
@@ -6,8 +6,10 @@ git pull
 
 # update knights of pen and paper
 # sometimes steamapps is SteamApps
+echo "updating Knights of Pen and Paper"
 test -r ~/.steam/steam/SteamApps && cp knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/SteamApps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
 test -r ~/.steam/steam/steamapps && cp knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
 
 # update FTL
+echo "updating FTL"
 cp faster_than_light/files/* ~/.local/share/FasterThanLight/
--- push_all.sh
+++ push_all.sh
@@ -2,10 +2,12 @@
 
 # update knights of pen and paper
 # for some reason steamapps is sometimes SteamApps
+echo "updating Knights of Pen and Paper"
 test -r ~/.steam/steam/SteamApps && cp ~/.steam/steam/SteamApps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp
 test -r ~/.steam/steam/steamapps && cp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp
 
 # update FTL
+echo "updating FTL"
 cp ~/.local/share/FasterThanLight/* faster_than_light/files/
 
 # push up to remote
