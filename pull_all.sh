12a32f0a35f7ea2030b1c708aca4630f5b833f58
Kimbsy <lordkimber@gmail.com>
2015-05-31 16:38:21 +0100

    fater than light capitalisation

--- pull_all.sh
+++ pull_all.sh
@@ -5,14 +5,16 @@ cd ~/Tools/save-files
 git pull
 
 # update knights of pen and paper
-# sometimes steamapps is SteamApps
+# sometimes SteamApps is steamapps
 echo "updating Knights of Pen and Paper"
 test -r ~/.steam/steam/SteamApps && cp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/SteamApps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
 test -r ~/.steam/steam/steamapps && cp ~/Tools/save-files/knights_of_pen_and_paper/files/data.kopp ~/.steam/steam/steamapps/common/Knights\ of\ Pen\ and\ Paper/knightspp_Data/data.kopp
 
 # update FTL
+# sometimes FasterThanLight is fasterthanlight
 echo "updating FTL"
-cp ~/Tools/save-files/faster_than_light/files/* ~/.local/share/FasterThanLight/
+test -r ~/.local/share/FasterThanLight/ && cp ~/Tools/save-files/faster_than_light/files/* ~/.local/share/FasterThanLight/
+test -r ~/.local/share/fasterthanlight/ && cp ~/Tools/save-files/faster_than_light/files/* ~/.local/share/fasterthanlight/
 
 # update Intrusion 2
 echo "updating Intrusion 2"
